440242d7a6f565603e9080ef7dc4fd5164d783dc
Carlymasslife <carlitacampos947@gmail.com>
2025-05-26 18:45:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+
+######### String ###############
+name="Aln" #esto es una variable
+age=11 #esto es una variable
+mensage="hola "+name+ " tienes " + str(age) #esto es una variable
+
+print (mensage)
+#para los espacios hay que ponenrlo espacios directamente dentro de las comillas
+########################################
+
+############# Integer #######
+number1=5
+number2=3
+sum=number1+number2
+resta= number2-number1
+multi= number2*number1
+dividir= number2/number1
+print(dividir)
+print(multi)
+
+print(resta)
+print(sum)
+
+
+
+# print(number1,number2)
+
+'''
+se puede sumar:
+- string + string
+-integer + integer
+- string NO integer
+'''
+
+###############  condicionales  ###################
+edad=18 
+if edad < 18:
+  print("eres menor de edad")
+if edad > 18:
+  print("eres mayor de edad")
+if edad==18: 
+  print("tienes 18 años")
+
+nombre="samu"
+if nombre=="sam":
+  print("Es mi profe")
+if nombre!="sam": #diferente
+  print("no es mi profe")
+
+'''
+tipos de comparaciones
+1. < menor 
+2. > mayor
+3. == igual
+4. = <- con esto no comparas nada, solo asignas un valor y daria error
+'''
+
+
+
